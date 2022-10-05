5f96cb4163a3cc57c9a8a18cce5387c341800f7c
sebneedles <sebneedles@gmail.com>
2022-10-05 11:33:03 +0200

    First commit

---
+++ app-kasa/src/App.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Kasa App</h1>
+      <h2>Start coding here ...</h2>
+    </div>
+  );
+}
