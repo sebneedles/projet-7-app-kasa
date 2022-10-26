1a3f152f26f2ecf2ceddb648c32d2ab476e010c3
sebneedles <sebneedles@gmail.com>
2022-10-26 14:33:27 +0200

    last commit

--- kasa/src/components/Card.js
+++ kasa/src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Card = ( {logement} ) => {
-    console.log(logement);
+    //console.log(logement);
     return (
         <div className="card" id={logement.id}>
             <Link to={`/lodging/${logement.id}`}>
